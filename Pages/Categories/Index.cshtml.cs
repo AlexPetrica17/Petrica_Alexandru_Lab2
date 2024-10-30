983af89c4947b3cfc0328ef24d47a2332db071e1
ALEXANDRU-NICOLAE PETRICA <alexandru.petrica@stud.ubbcluj.ro>
2024-10-30 17:52:44 +0200

    Laborator 4

--- Pages/Categories/Index.cshtml.cs
+++ Pages/Categories/Index.cshtml.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Lab2.Data;
 using Lab2.Models;
+using Lab2.Models.ViewModels;
+using System.Security.Policy;
 
 namespace Lab2.Pages.Categories
 {
@@ -21,9 +23,28 @@ namespace Lab2.Pages.Categories
 
         public IList<Category> Category { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public CategoriesIndexData CategoryData { get; set; }
+        public int CategoryID { get; set; }
+        public int BookID { get; set; }
+
+        public async Task OnGetAsync(int? id, int? bookID)
         {
-            Category = await _context.Category.ToListAsync();
+            //Category = await _context.Category.ToListAsync();
+            CategoryData = new CategoriesIndexData();
+            CategoryData.Categories = await _context.Category
+            .Include(i => i.BookCategories)
+            .ThenInclude(a => a.Book)
+            .ThenInclude(c => c.Author)
+            .OrderBy(i => i.CategoryName)
+            .ToListAsync();
+            if (id != null)
+            {
+                CategoryID = id.Value;
+                Category category = CategoryData.Categories
+                      .Where(c => c.ID == id.Value).Single();
+                CategoryData.Books = category.BookCategories
+                    .Select(bc => bc.Book);
+            }
         }
     }
 }
